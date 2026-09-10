e65511a7aee9cc755b4d51832cd8ba5b17b2cfce
Albian <albianseo@gmail.com>
2026-09-10 15:41:51 +0800

    Initial commit

---
+++ src/components/WelcomeMessage.tsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+  return (
+    <div>
+      <h2>Smart Ground-Truthing</h2>
+      <p>Digital biodiversity documentation system.</p>
+    </div>
+  )
+}
+
+export default WelcomeMessage
